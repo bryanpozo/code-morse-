b5b11b3e54073a5bfcafd6de642beb37aab26c76
bryanpozo <bryanmanuelmedinapozo2015@gmail.com>
2020-08-23 18:07:49 -0400

    Add files via upload

---
+++ codigomorse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Media;
+
+namespace codigomorse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insertar texto al convertir: ");
+            string mensaje;
+            mensaje = Console.ReadLine();
+            Console.WriteLine(Morsecode(mensaje));
+        }
+
+        static string Morsecode(string code)
+        {
+            string codex = code.ToUpper().Replace("A", ".-").Replace("B", " -... ").Replace("C", "-.-.").Replace("D", " -.. ").Replace("E", " . ").Replace("F", " ..-. ").Replace("G", " --. ").Replace("H", " .... ").Replace("I", " .. ").Replace("J", " .--- ").Replace("K", " -.- ").Replace("L", " .-.. ").Replace("M", " -- ").Replace("N", " -. ").Replace("O", " --- ").Replace("P", " .--. ").Replace("Q", " --.- ").Replace("R", " .-. ").Replace("S", " ... ").Replace("T", " - ").Replace("U", " ..- ").Replace("V", " •••- ").Replace("W", " .-- ").Replace("X", " -..- ").Replace("Y", " -.-- ").Replace("Z", " --.. ");
+
+            foreach (char item in codex)
+            {
+                if (item.ToString().Contains("."))
+                {
+                    Console.Beep(800, 200);
+                }
+
+                else if (item.ToString().Contains("-"))
+                {
+                    Console.Beep(800, 200 * 3);
+                }
+            }
+            return codex;
+        } 
+    } 
+}
